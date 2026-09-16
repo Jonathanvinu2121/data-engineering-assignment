c40f1caaabe5e038544cd071779ada9c4cdf899c
Jonathan T. Vinu <jonathanvinu2004@gmail.com>
2026-09-16 15:43:23 +0530

    Extract product details

--- q1/scraper.py
+++ q1/scraper.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote_plus
 
+
 search_term = input("Enter search term: ")
 
 encoded_term = quote_plus(search_term)
 
 url = f"https://mdcomputers.in/?route=product%2Fsearch&search={encoded_term}"
-
 
 response = requests.get(url)
 
@@ -15,8 +15,18 @@ print("Status:", response.status_code)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
+print(soup.title.text)
+
 products = soup.select(".product-grid-item")
 
 print("Products found:", len(products))
 
-print(soup.title.text)
+for product in products:
+    name = product.select_one(".product-entities-title a").get_text(strip=True)
+    url = product.select_one(".product-image-link")["href"]
+    price = product.select_one(".price .ins .amount").get_text(strip=True)
+
+    print("Name:", name)
+    print("Price:", price)
+    print("URL:", url)
+    print()
